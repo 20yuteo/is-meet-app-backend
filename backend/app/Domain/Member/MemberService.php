d11e64490066f670aaddc5845013e9eb5828f956
20yuteo <20yuteo@gmail.com>
2022-05-24 21:41:15 +0900

    ルームに既に同じ名前のユーザーがいるかチェックするように変更

--- backend/app/Domain/Member/MemberService.php
+++ backend/app/Domain/Member/MemberService.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Member;
 
 use App\Models\Member;
+use App\Models\Room;
 
 class MemberService {
 
@@ -12,7 +13,7 @@ class MemberService {
      * @return boolean $result
      * 
      */
-    public function checkExistsMemberName(string $name) {
-        return !Member::where('name', $name)->exists();
+    public function checkExistsMemberName(string $token, string $name) {
+        return !Room::where('token', $token)->first()->members()->where('name', $name)->exists();
     }
 }

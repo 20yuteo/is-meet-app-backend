11b5137eb68fb935b8c5b8c3688209dff8a6ee8f
20yuteo <20yuteo@gmail.com>
2022-06-09 21:11:34 +0900

    deploy.sh を修正

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,5 +1,5 @@
-docker compose exec app composer install
-docker compose exec app cp .env.prod .env
-docker compose exec app php artisan key:generate
-docker compose exec app php artisan storage:link
-docker compose exec app chmod -R 777 storage bootstrap/cache
+docker-compose exec app composer install
+docker-compose exec app cp .env.prod .env
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan storage:link
+docker-compose exec app chmod -R 777 storage bootstrap/cache
